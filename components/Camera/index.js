b48e8089942322c86d2b6db18cf5dbdc3a04e510
Huy19IT1 <huy2001nd@gmail.com>
2023-05-04 20:16:33 +0700

    update

--- components/Camera/index.js
+++ components/Camera/index.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { Button, Image, View, Platform, TextInput } from "react-native";
+import { Image, View, Platform, TextInput, Text } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import axios from "axios";
+import { Button, Icon } from "@rneui/themed";
 const BASE_URL =
-  "https://e3cc-2001-ee0-4c4c-9510-f490-27d6-4a18-556f.ngrok-free.app";
+  // "https://e3cc-2001-ee0-4c4c-9510-f490-27d6-4a18-556f.ngrok-free.app";
+  "http://192.168.1.4:8888";
 export default function ImagePickerExample() {
   const [image, setImage] = useState(null);
 
@@ -16,7 +18,9 @@ export default function ImagePickerExample() {
       return;
     }
 
-    const result = await ImagePicker.launchCameraAsync({});
+    const result = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+    });
 
     // Explore the result
     console.log(result);
@@ -31,7 +35,6 @@ export default function ImagePickerExample() {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
-      aspect: [4, 3],
       quality: 1,
     });
 
@@ -55,6 +58,8 @@ export default function ImagePickerExample() {
       method: "POST",
       url: BASE_URL + "/api/upload",
       headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
         "Content-Type": "multipart/form-data", // add this
       },
       data: datas,
@@ -71,17 +76,19 @@ export default function ImagePickerExample() {
   const [rs, setRS] = useState();
   useEffect(() => {
     if (!first) return;
-    const datas = new FormData();
 
-    datas.append("text", first);
-    datas.append("model", "RNN");
     axios({
       method: "POST",
       url: BASE_URL + "/api/predict",
-      data: datas,
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      data: { text: first, model: "RNN" },
     })
       .then((e) => {
         setRS(e?.data);
+        alert(e?.data);
       })
       .catch((e) => {
         alert(e?.response?.data?.message || e?.message);
@@ -89,13 +96,115 @@ export default function ImagePickerExample() {
   }, [first]);
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button title="Pick an image from camera roll" onPress={pickImage} />
-      <Button title="With camera " onPress={openCamera} />
-      {image && (
-        <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        // justifyContent: "center",
+        width: "100%",
+        // backgroundColor: "red",
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 30,
+          fontWeight: "bold",
+          marginTop: 40,
+        }}
+      >
+        Fake News Detection
+      </Text>
+      <View
+        style={{
+          marginTop: 50,
+          marginBottom: 20,
+          flexDirection: "row",
+          // backgroundColor: "yellow",
+        }}
+      >
+        <View style={{ marginRight: 50 }}>
+          <Button
+            onPress={openCamera}
+            icon={{
+              name: "camera",
+              type: "ant",
+              color: "#3C486B",
+              size: 60,
+            }}
+            type="outline"
+            buttonStyle={{
+              width: 100,
+              height: 100,
+              borderRadius: 100,
+              borderColor: "#3C486B",
+              borderWidth: 2,
+            }}
+          />
+          <Text style={{ textAlign: "center", marginTop: 10 }}>Take Photo</Text>
+        </View>
+        <View style={{ height: 150 }}>
+          <Button
+            onPress={pickImage}
+            icon={{
+              name: "photo",
+              type: "ant",
+              color: "#3C486B",
+              size: 60,
+            }}
+            type="outline"
+            buttonStyle={{
+              width: 100,
+              height: 100,
+              borderRadius: 100,
+              borderColor: "#3C486B",
+              borderWidth: 2,
+            }}
+          />
+          <Text style={{ textAlign: "center", marginTop: 10 }}>
+            Choose Photo
+          </Text>
+        </View>
+      </View>
+
+      <View
+        style={{
+          width: 400,
+          height: 200,
+          backgroundColor: "#F6F1F1",
+        }}
+      >
+        {image && (
+          <Image
+            source={{ uri: image }}
+            style={{ flex: 1, width: null, height: null }}
+            resizeMode="contain"
+          />
+        )}
+      </View>
+
+      {first && (
+        <TextInput
+          style={{ width: 400, height: 200, backgroundColor: "#F6F1F1" }}
+          value={first}
+          numberOfLines={10}
+          multiline={true}
+        />
       )}
-      {first && <TextInput value={first} numberOfLines={10} multiline={true} />}
+      {rs && (
+        <Text style={{ fontSize: 32, fontWeight: "bold" }}>
+          Predict:
+          {rs.split(";")[0] === "0"
+            ? "Real"
+            : rs.split(";")[0] === "2"
+            ? "No Text Selected"
+            : "Fake"}
+        </Text>
+      )}
+      {rs && (
+        <Text style={{ fontSize: 32, fontWeight: "bold" }}>
+          TOPIC:{rs.split(";")[1]}
+        </Text>
+      )}
     </View>
   );
 }
